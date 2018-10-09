7fac1e68c8fc81d11c1dbed60410d310ef1b571e
Jonathan Ström <jonathanstroem91@gmail.com>
2018-10-09 12:18:01 -0400

    Continuing work

--- python/transient-response/param-period.py
+++ python/transient-response/param-period.py
@@ -3,6 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('./data/transient-response/param-period.csv', header=4)
+def dp(t, period):
+    dp = 10.0*np.sin(2.0*np.pi*t/period) - 0.5
+    return dp
 
-print(df.pivot( columns='period', values='Attenuation factor'))
+df = pd.read_csv('./data/transient-response/param-period2.csv', header=4)
+
+
+
+df[['Time', 'period']].apply(lambda x: print(x[0][0]))
+#df['Pressure'] = df['']
+
+
+"""
+pivoted_df = df.pivot(
+    index='Time',
+    columns='period',
+    values='Attenuation factor',
+)
+
+pivoted_df.interpolate(
+    method='piecewise_polynomial',
+).plot(
+    logy=True,
+    logx=True,
+)
+# check what the average p is over each period, I think it should be higher for
+# the larger sin waves? could be interesting to see if you essentially achieve
+# the ss alpha value that corresponds to this average pressure value ?
+
+
+#plt.show()
+"""
