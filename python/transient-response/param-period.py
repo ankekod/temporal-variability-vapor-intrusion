5fcc0d98038efa403e7941a1fd60a6a685b02669
likgoek <jonathanstroem91@gmail.com>
2018-10-09 14:30:29 -0400

    Added reference lines, calculated pressure

--- python/transient-response/param-period.py
+++ python/transient-response/param-period.py
@@ -8,30 +8,58 @@ def dp(t, period):
     return dp
 
 df = pd.read_csv('./data/transient-response/param-period2.csv', header=4)
+df['Pressure'] = dp(df['Time'], df['period'])
 
 
 
-df[['Time', 'period']].apply(lambda x: print(x[0][0]))
-#df['Pressure'] = df['']
+df2 = pd.read_csv('data/preferential-pathway-sensitivity/param-preferential-pathway.csv', header=4)
+reference_lines = df2[((df2['chi'] == 1) & (df2['SB']==1)) & ((df2['p'] == 10.5) | (df2['p'] == -10.5))]
+
+print(reference_lines)
 
 
-"""
 pivoted_df = df.pivot(
     index='Time',
     columns='period',
-    values='Attenuation factor',
+    values=['Attenuation factor','Pressure'],
+).interpolate(
+    method='piecewise_polynomial',
 )
 
-pivoted_df.interpolate(
-    method='piecewise_polynomial',
-).plot(
+for period in df['period'].unique():
+    print(df['Pressure'][df['period']==period].median())
+
+fig, ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+
+pivoted_df.plot(
     logy=True,
-    logx=True,
+    #logx=True,
+    y='Attenuation factor',
+    #secondary_y='Pressure',
+    ax=ax1,
 )
+
+t0 = 0
+tau = pivoted_df.index.values[-1]
+
+
+ax1.semilogy(
+    [t0, tau],
+    [df2['alpha'].min(), df2['alpha'].min()],
+    'k--',
+)
+
+ax1.semilogy(
+    [t0, tau],
+    [df2['alpha'].max(), df2['alpha'].max()],
+    'k--',
+)
+
 # check what the average p is over each period, I think it should be higher for
 # the larger sin waves? could be interesting to see if you essentially achieve
 # the ss alpha value that corresponds to this average pressure value ?
 
 
-#plt.show()
-"""
+plt.show()
