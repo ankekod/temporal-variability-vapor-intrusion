ed6d1a7de1b8a5a704b76835166e735d6e0fbc5d
Jonathan Ström <jonathanstroem91@gmail.com>
2018-10-11 09:09:30 -0400

    Calculated expulsion rate

--- python/transient-response/param-period.py
+++ python/transient-response/param-period.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Ae = 12.0 # 1/day
+M = 131.38 # TCE g/mol
+V = 300.0 # basement volume m3
+
+def Ae(p):
+    return 12.0*p
 
 def dp(t, period):
     dp = 10.0*np.sin(2.0*np.pi*t/period) - 0.5
@@ -9,7 +15,7 @@ def dp(t, period):
 
 df = pd.read_csv('./data/transient-response/param-period2.csv', header=4)
 df['Pressure'] = dp(df['Time'], df['period'])
-
+df['TCE expulsion rate'] = df['TCE in indoor air']*Ae(df['Pressure'])*M*V
 
 
 df2 = pd.read_csv('data/preferential-pathway-sensitivity/param-preferential-pathway.csv', header=4)
@@ -21,7 +27,7 @@ print(reference_lines)
 pivoted_df = df.pivot(
     index='Time',
     columns='period',
-    values=['Attenuation factor','Pressure'],
+    values=['Attenuation factor','Pressure','TCE emission rate','TCE expulsion rate'],
 ).interpolate(
     method='piecewise_polynomial',
 )
@@ -31,12 +37,12 @@ for period in df['period'].unique():
 
 fig, ax1 = plt.subplots()
 
-ax2 = ax1.twinx()
+#ax2 = ax1.twinx()
 
 pivoted_df.plot(
     logy=True,
-    #logx=True,
-    y='Attenuation factor',
+    logx=True,
+    y=['TCE expulsion rate'],
     #secondary_y='Pressure',
     ax=ax1,
 )
@@ -45,13 +51,13 @@ t0 = 0
 tau = pivoted_df.index.values[-1]
 
 
-ax1.semilogy(
+ax1.loglog(
     [t0, tau],
     [df2['alpha'].min(), df2['alpha'].min()],
     'k--',
 )
 
-ax1.semilogy(
+ax1.loglog(
     [t0, tau],
     [df2['alpha'].max(), df2['alpha'].max()],
     'k--',
