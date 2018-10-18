2081382e6891e12d4c3d83495ac14d3063ba716a
likgoek <jonathanstroem91@gmail.com>
2018-10-18 14:47:22 -0400

    Created framework for analysing CSTR reponse times

--- python/transient-response/cstr-reponse.py
+++ python/transient-response/cstr-reponse.py
@@ -1,43 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
+from scipy.integrate import LSODA
 import sqlite3
 import pandas as pd
-"""
+
 # constants
-Ae = 0.5/3600.0 # air exchange rate 1/s
-V = 300.0 # basement volume m3
-D = 7.4e-6 # diffusivity of TCE in air m2/s
-L = 15.0e-2 # foundation slab thickness m
+Ae = 0.5 # air exchange rate 1/s
+V = 1.0 # basement volume m3
+data_dir = './data/preferential-pathway-sensitivity/'
 
-# variables
-w = lambda t: 10.0**(-6.0 + 2.0*np.sin(2.0*np.pi*t/period))
-c = 1.0e-2 # concentration assumed constant
 
-# functions
-Pe = lambda w: w*L/(2.0*D)
-n = lambda w, c: w*c/(1.0 - np.exp(-Pe(w)))
+pp = pd.read_csv(
+    data_dir + 'param-preferential-pathway.csv',
+    header=4,
+)
 
-def dudt(u, t):
+print(pp)
 
-    w = 10.0**(-6.0 + 2.0*np.sin(2.0*np.pi*t/period))
-    Pe = w*L/(2.0*D)
-    n = w*c/(1.0 - np.exp(-Pe))
-    dudt =  n/V - u*Ae
+def dudt(t, u):
+    dudt =  n(t)/V - u*Ae
     return dudt
 
-#dudt = lambda u, t: n(w,c)/V - u*Ae
+
+n_max, n0 = 10.0, 1.0
+y_max, y0 = n_max/V/Ae, n0/V/Ae
+
+t0 = 0.0
+tau = 50.0
+solver = LSODA(dudt, t0, [y0], tau)
+
+t_eq = 10.0
+t, y = [], []
+n = lambda t: n_max if (t <= t_eq) else n0
+while solver.y < 0.99*y_max:
+    t.append(solver.t)
+    y.append(solver.y)
+    solver.step()
+t_eq = solver.t
+while solver.y > 1.01*y0:
+    t.append(solver.t)
+    y.append(solver.y)
+    solver.step()
+t_org = solver.t
 
 
-# parameters
-period = 3600.0 # 1 hour period
+plt.plot(t, y)
 
-t = np.linspace(0,period)
+plt.plot([t[0], t[-1]], [y_max, y_max], 'k--')
+plt.plot([t[0], t[-1]], [y0, y0], 'k--')
 
-
-u0 = n(w(0), c)/V/Ae
-
-
-sol = odeint(dudt, u0, t)
-print(sol)
-"""
+plt.show()
