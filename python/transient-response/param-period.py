5790053cdbf82fe217a0a01c7de2a3652ada23e3
likgoek <jonathanstroem91@gmail.com>
2018-10-29 18:46:49 -0400

    Analysing with new ASU data

--- python/transient-response/param-period.py
+++ python/transient-response/param-period.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.integrate import LSODA
+from scipy.interpolate import interp1d
 import sqlite3
 import seaborn as sns
 
@@ -8,64 +10,93 @@ data_dir = './data/preferential-pathway-sensitivity/'
 #db_dir = '/home/jonathan/lib/vapor-intrusion-dbs/'
 db_dir = 'C://Users/jstroem/lib/vapor-intrusion-dbs/'
 
-db_asu = sqlite3.connect(db_dir + 'asu_house.db')
+db_asu = sqlite3.connect(db_dir + 'hill-afb.db')
 asu = pd.read_sql_query(
     "SELECT \
-        day AS Day, \
-        pressure_difference AS Pressure, \
-        tce_emission_rate, \
-        building_flow_rate, \
-        tce_groundwater, \
-        exchange_rate AS Ae \
+        time, \
+        concentration \
     FROM \
-        parameters; \
-    ", db_asu,
-).interpolate(limit_area='inside',limit=250).dropna()
-# removes data that is outside model data range
-asu['Pressure'] = -1.0*asu['Pressure']
-asu = asu[(asu['Pressure'] < 10) & (asu['Pressure'] > -10)]
-# removes cpm period from dataset
-cpm_start, cpm_end = 780.0, 1157.0
-asu = asu[(asu['Day'] < cpm_start) | (asu['Day'] > cpm_end)]
-# assigns a PP open/closed column (for sorting purposes)
-asu['PP'] = pd.Series(asu['Day'].apply(lambda x: 'Open' if x < cpm_start else 'Closed'))
-# calculates attentuation factor
-asu['Concentration'] = asu['tce_emission_rate']/asu['building_flow_rate']
-asu['Attenuation factor'] = asu['Concentration']/asu['tce_groundwater']
-asu['Ae'] *= 3600.0
+        \"td-basement\";",
+    db_asu,
+)
 
-fig, ax1 = plt.subplots()
+"""
+pressure = pd.read_sql_query(
+    "SELECT \
+        time, \
+        pressure \
+    FROM \
+        pressure_difference;",
+    db_asu,
+).time.apply(pd.to_datetime)
+"""
 
-ax2 = ax1.twinx()
+entry_rate = pd.read_sql_query(
+    "SELECT \
+        time, \
+        emission_rate \
+    FROM \
+        daily_emission_rate;",
+    db_asu,
+)
 
-asu[asu.PP == 'Closed'].plot(
-    x = 'Day',
-    y = 'Attenuation factor',
-    ax=ax1,
+tracer = pd.read_sql_query(
+    "SELECT \
+        time, \
+        air_exchange_rate AS Ae \
+    FROM \
+        tracer;",
+    db_asu,
+)
+
+phase3 = lambda df: df[(df.time - df.time.min())/np.timedelta64(1,'D') > 765.0].copy()
+
+dfs = [asu, entry_rate, tracer]
+
+for df in dfs:
+    df.time = df.time.apply(pd.to_datetime)
+    #df = phase3(df)
+
+
+asu = phase3(asu)
+entry_rate = phase3(entry_rate)
+tracer = phase3(tracer)
+
+#asu['concentration'] = asu['concentration'].apply(np.log10)
+
+asu['dcdt'] = asu['concentration'].diff()/(asu['time'].diff()/np.timedelta64(1,'h'))
+
+fig, ax = plt.subplots()
+
+asu.plot(
+    x='time',
+    y='concentration',
+    #ax=ax1,
     logy=True,
+    style='o',
+    ax=ax,
+)
+entry_rate.plot(
+    x='time',
+    y='emission_rate',
+    logy=True,
+    style='o',
+    #color='orange',
+    ax=ax,
 )
 
-asu[asu.PP == 'Closed'].plot(
-    x = 'Day',
-    y = 'Ae',
-    color='orange',
-    ax=ax2,
+#print(asu.time.min())
+
+"""
+def dudt(t, u):
+    dudt =  n/V - u*Ae
+    return dudt
+solver = LSODA(
+    dudt,
+    t0,
+    y0.values,
+    tau,
+    max_step=1.0,
 )
-plt.show()
 """
-fig, axarr = plt.subplots(2,2, sharey=True)
-
-i = 0
-for state in asu.PP.unique():
-    for var in ('Ae', 'Pressure'):
-        ax = axarr.flatten()[i]
-        sns.kdeplot(
-            data=asu[asu.PP==state][var],
-            data2=asu[asu.PP==state]['Attenuation factor'].apply(np.log10),
-            ax=ax,
-        )
-        #ax.set_title('%s, r= %1.1f' % (state, corr))
-        i += 1
-
 plt.show()
-"""
