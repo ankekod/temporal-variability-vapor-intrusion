79723a2f051a92edbd6113878fc2fa4b089cbcce
Jonathan Ström <jonathanstroem91@gmail.com>
2018-11-05 09:38:38 -0500

    Using new database and looking at cd/dt

--- python/transient-response/param-period.py
+++ python/transient-response/param-period.py
@@ -7,104 +7,26 @@ import sqlite3
 import seaborn as sns
 
 data_dir = './data/preferential-pathway-sensitivity/'
-#db_dir = '/home/jonathan/lib/vapor-intrusion-dbs/'
-db_dir = 'C://Users/jstroem/lib/vapor-intrusion-dbs/'
-
-db_asu = sqlite3.connect(db_dir + 'hill-afb.db')
-asu = pd.read_sql_query(
-    "SELECT \
-        time, \
-        concentration \
-    FROM \
-        \"td-basement\";",
-    db_asu,
-)
-
-"""
-pressure = pd.read_sql_query(
-    "SELECT \
-        time, \
-        pressure \
-    FROM \
-        pressure_difference;",
-    db_asu,
-).time.apply(pd.to_datetime)
-"""
-
-entry_rate = pd.read_sql_query(
-    "SELECT \
-        time, \
-        emission_rate \
-    FROM \
-        daily_emission_rate;",
-    db_asu,
-)
-
-tracer = pd.read_sql_query(
-    "SELECT \
-        time, \
-        air_exchange_rate AS Ae \
-    FROM \
-        tracer;",
-    db_asu,
-)
-
-phase3 = lambda df: df[(df.time - df.time.min())/np.timedelta64(1,'D') > 765.0].copy()
-
-dfs = [asu, entry_rate, tracer]
-
-for df in dfs:
-    df.time = df.time.apply(pd.to_datetime)
-    #df = phase3(df)
+db_dir = '/home/jonathan/lib/vapor-intrusion-dbs/'
+#db_dir = 'C://Users/jstroem/lib/vapor-intrusion-dbs/'
 
 
-asu = phase3(asu)
-entry_rate = phase3(entry_rate)
-tracer = phase3(tracer)
-"""
-freqs = ['4H','12H','1D','3D','1W','1M']
-fig, ax = plt.subplots()
+db = sqlite3.connect(db_dir + 'hill-afb.db')
 
-for freq in freqs:
-    resample = asu.resample(freq, on='time').mean().reset_index()
-    resample['concentration'] = resample['concentration'].apply(np.log10)
-    resample['dcdt'] = resample['concentration'].diff()#/(resample['time'].diff()/np.timedelta64(1,'D'))
+asu = pd.read_sql_query("SELECT * from daily_averages;", db)
+phases = pd.read_sql_query("SELECT * from phases;", db)
+asu.Ae *= 24.0
+asu.t = asu.t.apply(pd.to_datetime)
+phases.start = phases.start.apply(pd.to_datetime)
+phase = phases[(phases.cpm == 'off') & (phases.land_drain == 'closed')]
+filter = (asu['t'] > phase['start'].values[0])
+asu = asu.loc[filter]
 
-    sns.kdeplot(
-        data=resample['dcdt'].replace([np.inf, -np.inf], np.nan).dropna(),
-        ax=ax,
-    )
-plt.show()
+asu.t = (asu.t - asu.t.min())/np.timedelta64(1,'D')
+asu.t = asu.t.apply(float)
 
-#print(asu.time.min())
+#asu['c'] = asu['c'].apply(np.log10)
+asu['dcdt'] = asu['c'].diff()/asu['t'].diff()
 
-"""
-tracer.plot(
-    x='time',
-    y='Ae',
-)
 
-asu.plot(
-    x='time',
-    y='concentration',
-    logy=True,
-)
-plt.show()
-Ae = interp1d(tracer.time, tracer.Ae)
-V = 350.0
-t0 = entry_rate.time.min()#/np.timedelta64(1,'D')
-print(t0)
-tau = entry_rate.time.max()
-
-y0 = entry_rate[entry_rate['time'] == t0]/V/Ae(t0)
-print(y0)
-def dudt(t, u):
-    dudt =  n/V - u*Ae
-    return dudt
-solver = LSODA(
-    dudt,
-    t0,
-    y0,
-    tau,
-    max_step=np.timedelta64(1,'D'),
-)
+print(asu['dcdt'].mean())
