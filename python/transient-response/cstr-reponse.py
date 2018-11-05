a67d38115dd18a612f556b049686cdf31d472171
Jonathan Ström <jonathanstroem91@gmail.com>
2018-11-05 09:38:51 -0500

    Cleaned up old stuff, set up new analysis

--- python/transient-response/cstr-reponse.py
+++ python/transient-response/cstr-reponse.py
@@ -11,193 +11,95 @@ import plotly.io as pio
 data_dir = './data/preferential-pathway-sensitivity/'
 figures_dir = './figures/transient-response/'
 
-def process_data(data):
-    data.n *= 3600.0 # converts mol/s to mol/hr
-    n_func = interp1d(data.p.values, data.n.values) # interpolation function
-    # all possible pressures and air exchanges rates
-    ps = np.arange(-10,11)
-    Aes = np.arange(0.1, 1.6, 0.1)
-    # calculates the indoor air concentration and puts all the input variables
-    # and concentration into lists
-    p, Ae, c, n = [], [], [], []
-    for Ae_now in Aes:
-        for p_now in ps:
-                Ae.append(Ae_now)
-                p.append(p_now)
-                c.append(n_func(p_now)/V/Ae_now)
-                n.append(n_func(p_now))
-    # puts data into dataframe
-    df = pd.DataFrame(
-        {
-        'p': p,
-        'Ae': Ae,
-        'c': c,
-        'n': n,
-        }
-    )
-    # reference state
-    ref_state = ((df.p == -2) & (df.Ae == 0.1))
-    ref = df[ref_state].copy()
-    # potential target states
-    target_state = ((ref.c.values/df.c >= 10.0) | (ref.c.values/df.c <= 0.1)) & (df.p % 5 == 0) & (df.p != 0)# & (df.Ae == 1.0)
-    target = df[target_state].copy()
-    return df, ref, target
-
-def plot_contour(df, ref, target):
-    # data to plot
-    data = [
-        go.Contour(
-            z=df.c.apply(np.log10),
-            x=df.p,
-            y=df.Ae,
-        ),
-        go.Scatter(
-            x = ref.p,
-            y = ref.Ae
-        ),
-        go.Scatter(
-            x = target.p,
-            y = target.Ae,
-            mode = 'markers',
-        ),
-    ]
-    # figure
-    fig = go.Figure(
-        data=data,
-        #layout=layout,
-    )
-    # saving data
-    filename = 'p-ae-contour'
-    pio.write_image(
-        fig,
-        file = figures_dir + filename + '.pdf',
-        )
-    return
-
-
-
-def solve_cstr(data):
-    # unsteady cstr method
-    def dudt(t, u):
-        dudt =  n/V - u*Ae
-        return dudt
-    #retrives processed data
-    df, ref, target = process_data(data)
-    # time variables
-    t0 = 0.0 # initial time
-    tau = 128 # max allowed time
-    # initial/reference concentration
-    y0 = ref.n/V/ref.Ae
-    for n, Ae, p in zip(target.n, target.Ae, target.p):
-        # solving for target state change in variable values
-        print('Case: p = %1.1f, Ae = %1.1f' % (p, Ae))
-        c = n/V/Ae
-        solver = LSODA(
-            dudt,
-            t0,
-            y0.values,
-            tau,
-            max_step=1.0,
-        )
-        t, y = [], [] # storage lists
-        while solver.y > 1.01*c:
-            t.append(solver.t)
-            y.append(solver.y)
-            try:
-                solver.step()
-            except:
-                print('Solver failed in first loop at t = %1.1f' % solver.t)
-                break
-        t_eq = solver.t
-        print('Min. reached after %2.1f hours' % t_eq)
-        # going back to reference state variables
-        Ae_label = Ae
-        Ae = ref.Ae.values
-        while solver.y < 0.99*y0.values:
-            t.append(solver.t)
-            y.append(solver.y)
-            try:
-                solver.step()
-            except:
-                print('Solver failed in second loop at t = %1.1f' % solver.t)
-                break
-        t_org = solver.t
-        print('Max. reached after %2.1f hours' % t_org)
-
-        plt.semilogy(
-            np.array(t),
-            y,
-            label='p = %i, Ae = %1.1f, t_dcrs = %1.1f, t_incr = %1.1f' % (p, Ae_label, t_eq, t_org),
-        )
-
-    plt.legend()
-    plt.show()
-    return
-
-
-def plot_ribbon(data):
-
-    df, ref, target = process_data(data)
-
-
-    traces = []
-
-
-    traces.append(
-        dict(
-            x=df.p.values,
-            y=df.Ae.values,
-            z=df.n.values/df.Ae.values/V,
-            type='surface',
-            showscale=False,
-        )
-    )
-    """
-    y_raw = spectra[:, 0] # wavelength
-    sample_size = df.shape[1]-1
-    for i in range(1, sample_size):
-        z_raw = spectra[:, i]
-        x = []
-        y = []
-        z = []
-        ci = int(255/sample_size*i) # ci = "color index"
-        for j in range(0, len(z_raw)):
-            z.append([z_raw[j], z_raw[j]])
-            y.append([y_raw[j], y_raw[j]])
-            x.append([i*2, i*2+1])
-        traces.append(dict(
-            z=z,
-            x=x,
-            y=y,
-            colorscale=[ [i, 'rgb(%d,%d,255)'%(ci, ci)] for i in np.arange(0,1.1,0.1) ],
-            showscale=False,
-            type='surface',
-        ))
-    """
-    # figure
-    fig = go.Figure(
-        data=traces,
-        #layout=layout,
-    )
-    # saving data
-    filename = 'p-ae-ribbon'
-    pio.write_image(
-        fig,
-        file = figures_dir + filename + '.svg',
-        )
-    return
-
-
 # parameters
 V = 200.0
+df = pd.read_csv('./data/soils/all-data.csv')
 
-# reads data file
-data = pd.read_csv(
-    data_dir + 'param-no-preferential-pathway.csv',
-    header=4,
+Ae = 0.5
+
+t1, t2 = [], []
+dc1, dc2 = [], []
+Aes = []
+dcdt1, dcdt2 = [], []
+soils = []
+fig, (ax1, ax2) = plt.subplots(1,2)
+
+df.n *= 3600.0
+for Ae in [0.5, 1.0, 0.1]:
+
+    for soil in df['soil_type'].unique():
+        ref = df[ (df['soil_type'] == soil) & ( df.p==-5 ) ]
+        target = df[ (df['soil_type'] == soil) & ( df.p==5 ) ]
+        Aes.append(Ae)
+        soils.append(soil)
+        # unsteady cstr method
+        def dudt(t, u):
+            dudt =  n/V - u*Ae
+            return dudt
+        #retrives processed data
+        # time variables
+        t0 = 0.0 # initial time
+        tau = 240 # max allowed time
+        # initial/reference concentration
+        y0 = ref.n/V/Ae
+        for n, p in zip(target.n, target.p):
+            # solving for target state change in variable values
+            #print('Case: p = %1.1f, Ae = %1.1f' % (p, Ae))
+            c = n/V/Ae
+            solver = LSODA(
+                dudt,
+                t0,
+                y0.values,
+                tau,
+                max_step=0.1,
+            )
+            t, y = [], [] # storage lists
+            while solver.y > 1.01*c:
+                t.append(solver.t)
+                y.append(solver.y)
+                try:
+                    solver.step()
+                except:
+                    print('Solver failed in first loop at t = %1.1f' % solver.t)
+                    break
+            t_eq = solver.t
+            t1.append(t_eq)
+            dc1.append((c-y0))
+            dcdt1.append((c-y0)/t_eq)
+            print('Min. reached after %2.1f hours' % t_eq)
+            # going back to reference state variables
+            n = ref.n
+            while solver.y < 0.99*y0.values:
+                t.append(solver.t)
+                y.append(solver.y)
+                try:
+                    solver.step()
+                except:
+                    print('Solver failed in second loop at t = %1.1f' % solver.t)
+                    break
+            t_org = solver.t - t_eq
+            t2.append(t_org)
+            dc2.append((y0-c))
+            dcdt2.append((y0-c)/t_org)
+            print('Max. reached after %2.1f hours' % t_org)
+
+
+for var in [soils,Aes,t1,t2,dc1,dc2,dcdt1,dcdt2]:
+    print(len(var))
+
+df2 = pd.DataFrame(
+    {'soil': soils,
+    'Ae': Aes,
+    't1': t1,
+    't2': t2,
+    'dc1': dc1,
+    'dc2': dc2,
+    'dcdt1': dcdt1,
+    'dcdt2': dcdt2}
 )
-# only interested in the case with a gravel sub-base and no contaminant in the
-# pp
-data = data[(data.SB == 1) & (data.chi == 0)]
-solve_cstr(data)
-#plot_ribbon(data)
+
+df2.plot(x='soil',y='t1',ax=ax1, label='Ae = %1.1f' % Ae)
+df2.plot(x='soil',y='t2',ax=ax2,  label='Ae = %1.1f' % Ae)
+
+plt.legend() # implementera soil type, dc = x 
+plt.show()
