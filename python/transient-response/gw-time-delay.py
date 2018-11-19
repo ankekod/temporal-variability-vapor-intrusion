aacc1382568607c23ea4bdf78dcfce3da39d4cde
likgoek <jonathanstroem91@gmail.com>
2018-11-19 15:28:05 -0500

    Testing around optimizing around phase 3 instead of the whole dataset

--- python/transient-response/gw-time-delay.py
+++ python/transient-response/gw-time-delay.py
@@ -7,8 +7,8 @@ import sqlite3
 import seaborn as sns
 from scipy.stats import pearsonr
 
-db_dir = '/home/jonathan/lib/vapor-intrusion-dbs/'
-#db_dir = 'C://Users/jstroem/lib/vapor-intrusion-dbs/'
+#db_dir = '/home/jonathan/lib/vapor-intrusion-dbs/'
+db_dir = 'C://Users/jstroem/lib/vapor-intrusion-dbs/'
 
 db = sqlite3.connect(db_dir + 'hill-afb.db')
 
@@ -27,39 +27,69 @@ asu = pd.read_sql_query(
     WHERE avgs.time IS NULL;",
     db,
 ).sort_values('time').reset_index().interpolate(method='piecewise_polynomial').dropna()
+K_H = 0.403
+phases = pd.read_sql_query("SELECT * from phases;", db)
+phase3 = phases[(phases.cpm == 'off') & (phases.land_drain == 'closed')]
+
+
+
 
 asu.time = asu.time.apply(pd.to_datetime)
+filter3 = (asu['time'] > phase3['start'].values[0])
 
 shifts = np.arange(0,len(asu)-1)
 r = []
 dt = []
+var = []
 fig, ax = plt.subplots()
 
 for shift in shifts:
     df = asu.copy()
     df.gw_concentration = df.gw_concentration.shift(shift)
+    df['alpha'] = df['concentration']/(df['gw_concentration']*1e3*K_H)
+    df['alpha'] = df['alpha'].apply(np.log10)
+    df = df.loc[filter3]
     df = df.dropna()
-    r.append(pearsonr(df.gw_concentration,df.concentration)[0])
+    r.append(pearsonr(df.concentration,df.gw_concentration)[0])
+    var.append(df['alpha'].std())
     #df.plot(x='time',y=['concentration','gw_concentration'],logy=True,ax=ax,style=['-','--'],label=['Concentation, shift = %i' % shift, 'GW, shift = %i' % shift])
     dt.append(df.time.min() - asu.time.min())
 
 
-time_delay = pd.DataFrame({'dt': dt, 'r': r})
+time_delay = pd.DataFrame({'dt': dt, 'r': r, 'var': var})
 
 
 time_delay.dt /= np.timedelta64('1','D')
-#time_delay.plot(y='r',ax=ax)
+time_delay.plot(y='r',ax=ax)
 
 asu_shifted = asu.copy()
-asu_shifted['gw_concentration'] = asu_shifted['gw_concentration'].shift(125)
+asu_shifted['gw_concentration'] = asu_shifted['gw_concentration'].shift(153)
 
-K_H = 0.403
+
 
 
 asu['alpha'] = asu['concentration']/(asu['gw_concentration']*1e3*K_H)
 asu_shifted['alpha'] = asu_shifted['concentration']/(asu_shifted['gw_concentration']*1e3*K_H)
 
+
+fig, ax = plt.subplots()
+
+
+asu = asu.loc[filter3]
+asu_shifted = asu_shifted.loc[filter3]
+
 asu.plot(x='time',y='alpha',logy='True',ax=ax,label='ASU')
 asu_shifted.plot(x='time',y='alpha',logy='True',ax=ax,label='ASU Shifted')
+
+
+
+fig, ax = plt.subplots()
+asu['alpha'] = asu['alpha'].apply(np.log10)
+asu_shifted['alpha'] = asu_shifted['alpha'].apply(np.log10)
+
+
+sns.kdeplot(asu['alpha'],ax=ax,label='ASU')
+sns.kdeplot(asu_shifted['alpha'],ax=ax,label='ASU Shifted')
+
 
 plt.show()
