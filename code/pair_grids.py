f73bb5ceab11bc08e1497a5cfcfc9c2dfaa70516
likgoek <jonathanstroem91@gmail.com>
2019-02-19 15:28:40 -0500

    Beginning pairgrid plots

---
+++ code/pair_grids.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+
+
+asu = pd.read_csv('./data/asu_house.csv')
+indianapolis = pd.read_csv('./data/indianapolis.csv')
+
+
+# demonstrate the variability
+
+
+# asu
+# PP hue
+g = sns.PairGrid(
+    asu.loc[asu['Phase']!='CPM'][['IndoorOutdoorPressure','AirExchangeRate','logIndoorConcentration','logAttenuationSubslab','Phase']],
+    hue='Phase',
+)
+g = g.map_upper(plt.scatter)
+g = g.map_lower(sns.regplot, x_bins=20, truncate=True, )
+g = g.map_diag(sns.kdeplot, shade=True)
+g = g.add_legend()
+
+
+# season hue
+g = sns.PairGrid(
+    asu.loc[asu['Phase']=='Closed'][['IndoorOutdoorPressure','AirExchangeRate','logIndoorConcentration','logAttenuationSubslab','Season']],
+    hue='Season',
+    hue_order=['Winter', 'Fall', 'Spring', 'Summer'],
+)
+g = g.map_upper(plt.scatter)
+g = g.map_lower(sns.regplot, x_bins=20, truncate=True, )
+g = g.map_diag(sns.kdeplot, shade=True)
+g = g.add_legend()
+
+
+# indianapolis
+# specie hue
+g = sns.PairGrid(
+    indianapolis[['IndoorOutdoorPressure','logIndoorConcentration','logAttenuationSubslab','Specie']],
+    hue='Specie',
+)
+g = g.map_upper(plt.scatter)
+g = g.map_lower(sns.regplot, x_bins=20, truncate=True, )
+g = g.map_diag(sns.kdeplot, shade=True)
+g = g.add_legend()
+
+# season hue
+g = sns.PairGrid(
+    indianapolis.loc[indianapolis['Specie']=='Tetrachloroethene'][['IndoorOutdoorPressure','logIndoorConcentration','logAttenuationSubslab','Season']],
+    hue='Season',
+    hue_order=['Winter', 'Fall', 'Spring', 'Summer'],
+)
+g = g.map_upper(plt.scatter)
+g = g.map_lower(sns.regplot, x_bins=20, truncate=True, )
+g = g.map_diag(sns.kdeplot, shade=True)
+g = g.add_legend()
+
+plt.show()
