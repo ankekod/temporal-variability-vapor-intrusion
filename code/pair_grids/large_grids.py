6469aeb27b0aa856eb8736a3e80082ab0fd57e9f
Jonathan Ström <jonathanstroem91@gmail.com>
2019-03-07 07:56:55 -0500

    Splitting and organising

---
+++ code/pair_grids/large_grids.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.gridspec as gridspec
+from scipy import stats
+
+
+
+asu = pd.read_csv('./data/asu_house.csv')
+indianapolis = pd.read_csv('./data/indianapolis.csv')
+
+# data processing
+asu = asu.loc[(asu['IndoorOutdoorPressure'] >= -5) & (asu['IndoorOutdoorPressure'] <= 5)] # TODO: kind of ugly "hack", should instead adjust axes
+
+# global settings
+num_bins = 10
+fig_path = './figures/pair_grids/'
+ext = '.pdf'
+dpi = 300
+
+# method to set custom axis labels
+def custom_axis(g):
+
+    replacements = {
+        'IndoorOutdoorPressure': '$\\Delta p_\\mathrm{in/out}$ (Pa)',
+        'AirExchangeRate': '$A_e$ (1/h)',
+        'logIndoorConcentration': '$\\log{(c_\\mathrm{in})}$ ($\\mathrm{\\mu g/m^3}$)',
+        'logAttenuationSubslab': '$\\log{(\\alpha_\\mathrm{subslab})}$'
+    }
+
+    for i in range(len(g.axes)):
+        for j in range(len(g.axes)):
+            xlabel = g.axes[i][j].get_xlabel()
+            ylabel = g.axes[i][j].get_ylabel()
+            if xlabel in replacements.keys():
+                g.axes[i][j].set_xlabel(replacements[xlabel])
+            if ylabel in replacements.keys():
+                g.axes[i][j].set_ylabel(replacements[ylabel])
+    return g
+
+
+# demonstrate the variability
+
+# asu
+# PP hue
+g = sns.PairGrid(
+    asu.loc[asu['Phase']!='CPM'][['IndoorOutdoorPressure','AirExchangeRate','logIndoorConcentration','logAttenuationSubslab','Phase']],
+    hue='Phase',
+)
+g = g.map_upper(plt.scatter)
+g = g.map_lower(sns.regplot, x_bins=num_bins, truncate=True, )
+g = g.map_diag(sns.kdeplot, shade=True)
+g = g.add_legend()
+g = custom_axis(g)
+plt.savefig(fig_path+'asu_phase'+ext,dpi=dpi)
+
+# season hue
+for phase in asu['Phase'].unique():
+    try:
+        g = sns.PairGrid(
+            asu.loc[asu['Phase']==phase][['IndoorOutdoorPressure','AirExchangeRate','logIndoorConcentration','logAttenuationSubslab','Season']],
+            hue='Season',
+            hue_order=['Winter', 'Fall', 'Spring', 'Summer'],
+        )
+        g = g.map_upper(plt.scatter)
+        g = g.map_lower(sns.regplot, x_bins=num_bins, truncate=True, )
+        g = g.map_diag(sns.kdeplot, shade=True)
+        g = g.add_legend()
+        g = custom_axis(g)
+        g.fig.suptitle('Land drain ' + phase.lower())
+        plt.savefig(fig_path+'asu_season_'+phase.lower()+ext,dpi=dpi)
+    except:
+        continue
+
+# indianapolis
+# specie hue
+g = sns.PairGrid(
+    indianapolis[['IndoorOutdoorPressure','logIndoorConcentration','logAttenuationSubslab','Specie']],
+    hue='Specie',
+)
+g = g.map_upper(plt.scatter)
+g = g.map_lower(sns.regplot, x_bins=num_bins, truncate=True, )
+g = g.map_diag(sns.kdeplot, shade=True)
+g = g.add_legend()
+g = custom_axis(g)
+plt.savefig(fig_path+'indianapolis_species'+ext,dpi=dpi)
+
+# season hue
+for specie in indianapolis['Specie'].unique():
+    g = sns.PairGrid(
+        indianapolis.loc[indianapolis['Specie']==specie][['IndoorOutdoorPressure','logIndoorConcentration','logAttenuationSubslab','Season']],
+        hue='Season',
+        hue_order=['Winter', 'Fall', 'Summer'],
+    )
+    g = g.map_upper(plt.scatter)
+    g = g.map_lower(sns.regplot, x_bins=num_bins, truncate=True, )
+    g = g.map_diag(sns.kdeplot, shade=True)
+    g = g.add_legend()
+    g = custom_axis(g)
+    g.fig.suptitle(specie)
+    plt.savefig(fig_path+'indianapolis_season_'+specie.lower()+ext,dpi=dpi)
