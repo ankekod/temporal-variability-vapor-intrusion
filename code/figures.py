e6ecdea7761b26b8fd55711d791ea8b3e362169b
likgoek <jonathanstroem91@gmail.com>
2019-03-08 15:00:38 -0500

    Making new 2D KDE plot

--- code/figures.py
+++ code/figures.py
@@ -4,39 +4,86 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.gridspec as gridspec
 from scipy import stats
-from get_simulation_data import Soil
+from get_simulation_data import Soil, PreferentialPathway
 
 
-# TODO: Run new transient simulation with NAS pressure data?
+"""
+This figure shows a case (North Island NAS) where indoor/outdoor pressure
+difference the key driver at a VI site, and how are are able to model this
+by assuming a permeable soil type (sand).
+"""
 class Figure1:
-    def __init__(self):
-        data = pd.read_csv('./data/north_island.csv').dropna()
-        sim = Soil().data
-        sim = sim.loc[(sim['SoilType']=='Sand')]
+    def __init__(self,y_data_log=False , norm_conc=True):
+        nas = pd.read_csv('./data/north_island.csv').dropna()
+        asu = pd.read_csv('./data/asu_house.csv').dropna()
+        asu_open = asu.loc[asu['Phase']=='Open']
+        asu_closed = asu.loc[asu['Phase']=='Closed']
 
 
-        ax = sns.kdeplot(
-            data=data['IndoorOutdoorPressure'],
-            data2=data['logIndoorConcentration']-data['logIndoorConcentration'].mean(),
-            shade_lowest=False,
-            shade=True,
-        )
-        # TODO: Normalise to when Pa = 0
-        sim['logAttenuationGroundwaterFromMean'] = sim['logAttenuationGroundwater'] - sim.loc[sim['IndoorOutdoorPressure']==-1]['logAttenuationGroundwater'].values
-        ax = sns.lineplot(
-            data=sim,
-            x='IndoorOutdoorPressure',
-            y='logAttenuationGroundwaterFromMean',
-            ax=ax,
-        )
+        x='IndoorOutdoorPressure'
 
+        if y_data_log is True:
+            y='logIndoorConcentration'
+        else:
+            y='IndoorConcentration'
+
+
+        datasets = (nas, asu_open, asu_closed,)
+        labels = ('North Island NAS', 'ASU house, land drain open', 'ASU house, land drain closed',)
+
+        fig, ax = plt.subplots()
+
+        for data, label in zip(datasets, labels):
+            if norm_conc is True:
+                if y_data_log is True:
+                    data2 = data[y]-data[y].mean()
+                else:
+                    data2 = data[y]/data[y].mean()
+
+            else:
+                data2 = data[y]
+
+
+            r, p = stats.pearsonr(data[x], data2)
+            sns.kdeplot(
+                data=data[x],
+                data2=data2,
+                shade_lowest=False,
+                shade=True,
+                label=label + ', r = %1.2f' % r, # TODO: Add Pearson's r analysis
+                ax=ax,
+            )
+
+
+
+        yticks, yticklabels = get_log_ticks(-1, 1.5)
+        print(yticklabels)
+        # formatting options
         ax.set(
             xlim=[-30,15],
+            ylim=[-1,1.5],
+            xlabel='$p_\\mathrm{in/out} \\; \\mathrm{(Pa)}$',
+            ylabel='$\\log{(c_\\mathrm{in})} \\; \\mathrm{(\\mu g/m^3)}$',
+            title='Relationship between indoor/outdoor pressure difference and\nTCE in indoor air (normalized to mean concentration)',
+            #yscale='log',
+            yticks=yticks,
+            yticklabels=yticklabels,
         )
-
+        plt.legend(loc='upper left')
         plt.show()
 
         return
 
 
-Figure1()
+def get_log_ticks(start, stop):
+
+    ticks = np.array([])
+    for int_now in np.arange(np.floor(start),np.ceil(stop)+1):
+        ticks = np.append(ticks, np.arange(0.1,1.1,0.1)*10.0**int_now)
+
+    labels = ['%1.1f' % tick for tick in ticks]
+    ticks = np.log10(ticks)
+
+
+    return ticks, labels
+Figure1(y_data_log=True,norm_conc=True)
