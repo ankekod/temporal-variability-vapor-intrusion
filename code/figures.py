8784b2c5e43dc820a8a2fb3751055333f664d578
Jonathan Ström <jonathanstroem91@gmail.com>
2019-03-07 20:39:51 -0500

    Working on new figures

---
+++ code/figures.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.gridspec as gridspec
+from scipy import stats
+from get_simulation_data import Soil
+
+
+# TODO: Run new transient simulation with NAS pressure data?
+class Figure1:
+    def __init__(self):
+        data = pd.read_csv('./data/north_island.csv').dropna()
+        sim = Soil().data
+        sim = sim.loc[(sim['SoilType']=='Sand')]
+
+        print(sim)
+
+        ax = sns.kdeplot(
+            data=data['IndoorOutdoorPressure'],
+            data2=data['logIndoorConcentration']-data['logIndoorConcentration'].mean(),
+            shade_lowest=False,
+            shade=True,
+        )
+
+
+
+        sim['logAttenuationGroundwaterFromMean'] = sim['logAttenuationGroundwater'] - sim['logAttenuationGroundwater'].median()
+        ax = sns.lineplot(
+            data=sim,
+            x='IndoorOutdoorPressure',
+            y='logAttenuationGroundwaterFromMean',
+            ax=ax,
+        )
+
+        ax.set(
+            xlim=[-30,15],
+        )
+
+        plt.show()
+
+        return
+
+
+Figure1()
---
+++ code/get_simulation_data.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+
+# TODO: Create class with common functions
+
+class PreferentialPathway:
+    def __init__(self):
+        dfs = []
+        for file in ('pp','pp_uniform','no_pp',):
+            df = pd.read_csv('./data/simulation/sweep_'+file+'.csv',header=4)
+            df['Simulation'] = np.repeat(file.replace('_',' ').title(),len(df))
+            dfs.append(df)
+
+        df = pd.concat(dfs)
+        df.rename(
+            columns={
+                '% Ae': 'AirExchangeRate',
+                'p_in': 'IndoorOutdoorPressure',
+                'Attenuation to groundwater, Global Evaluation: Attenuation to groundwater {gev2}': 'AttenuationGroundwater',
+                'Global Evaluation: Relative air entry rate {gev3}': 'RelativeAirEntryRate',
+                'Average crack Peclet number, Global Evaluation: Crack Peclet number {gev4}': 'Peclet',
+                'TCE in indoor air, Global Evaluation: TCE in indoor air {gev5}': 'IndoorConcentration',
+                'Global Evaluation: TCE emission rate {gev6}': 'EntryRate',
+                'Attenuation to subbase, Global Evaluation: Attenuation to subslab {gev9}': 'AttenuationSubslab',
+                'Global Evaluation: Average TCE in subslab {gev10}': 'SubslabConcentration',
+            },
+            inplace=True,
+        )
+
+
+        # data choosing
+        df['AirExchangeRate'] *= 3600 # convert from 1/s to 1/hr
+        df['logIndoorConcentration'] = df['IndoorConcentration'].apply(np.log10)
+        df['logAttenuationSubslab'] = df['AttenuationSubslab'].apply(np.log10)
+        df['logAttenuationGroundwater'] = df['AttenuationGroundwater'].apply(np.log10)
+
+
+        self.data = df
+        return
+
+class Soil:
+    def __init__(self):
+
+        df = pd.read_csv('./data/soils/all-data.csv')
+
+        df.drop(columns=('Unnamed: 0'),inplace=True)
+
+        df.rename(
+            columns={
+                'p': 'IndoorOutdoorPressure',
+                'alpha': 'AttenuationGroundwater',
+                'beta': 'RelativeAirEntryRate',
+                'Pe': 'Peclet',
+                'c': 'IndoorConcentration',
+                'n': 'EntryRate',
+                'soil_type': 'SoilType',
+            },
+            inplace=True,
+        )
+
+        df['SoilType']=df['SoilType'].map(lambda x: x.title().replace('-', ' '))
+        # data choosing
+        df['logIndoorConcentration'] = df['IndoorConcentration'].apply(np.log10)
+        df['logAttenuationGroundwater'] = df['AttenuationGroundwater'].apply(np.log10)
+
+        self.data = df
+
+        return
