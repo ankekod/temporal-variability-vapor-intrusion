d27c20013d09367db523d7f937f5167f21badb41
likgoek <jonathanstroem91@gmail.com>
2019-04-02 11:28:02 -0400

    Implemented Eric's suggestions

--- code/figures.py
+++ code/figures.py
@@ -82,12 +82,20 @@ class PressureKDE:
 
 def get_log_ticks(start, stop, style='e'):
 
+    def smart_strip(str):
+        for i, x in enumerate(str):
+            str[i] = x.rstrip('0')
+            if str[i][-1] == '.':
+
+                str[i] += '0'
+
+        return str
     ticks = np.array([])
     ints = np.arange(np.floor(start),np.ceil(stop)+1)
     for int_now in ints:
-        ticks = np.append(ticks, np.arange(0.1,1.1,0.1)*10.0**int_now)
-        ticks = np.unique(ticks)
-
+        ticks = np.append(ticks, np.arange(0.1,1.0,0.1)*10.0**int_now)
+        #ticks = np.unique(ticks)
+    ticks = np.append(ticks, 1.0*10.0**ints[-1])
 
     if style=='e':
         labels = ['%1.1e' % tick for tick in ticks]
@@ -96,10 +104,10 @@ def get_log_ticks(start, stop, style='e'):
         labels = ['%1.12f' % tick for tick in ticks]
         ticks_to_keep = ['%1.12f' % 10**int for int in ints]
 
+    ticks_to_keep = np.unique(ticks_to_keep)
+
     ticks = np.log10(ticks)
 
-    labels = list(map(lambda x: x.rstrip('0'), labels))
-    ticks_to_keep = list(map(lambda x: x.rstrip('0'), ticks_to_keep))
 
     for i, label in enumerate(labels):
 
@@ -109,6 +117,8 @@ def get_log_ticks(start, stop, style='e'):
         else:
             #print('Removing label')
             labels[i] = ' '
+
+    labels = smart_strip(labels)
     return ticks, labels
 
 
@@ -117,14 +127,20 @@ class AttenuationSubslab:
         asu = pd.read_csv('./data/asu_house.csv')
         asu = asu.loc[asu['Phase']!='CPM']
 
-        ax = sns.boxplot(x="Phase", y="logAttenuationSubslab", data=asu)
+        ax = sns.boxplot(
+            x="Phase",
+            y="logAttenuationSubslab",
+            data=asu,
+            whis=10,
+            )
 
-        ticks, labels = get_log_ticks(-4,2, style='e')
+
+        ticks, labels = get_log_ticks(-3,2, style='f')
         ax.set(
             xlabel='Preferential pathway status',
             ylabel='Attenuation from subslab',
-            #yticks=ticks,
-            #yticklabels=labels,
+            yticks=ticks,
+            yticklabels=labels,
         )
 
         plt.tight_layout()
@@ -454,20 +470,7 @@ class Diurnal:
 
 #Diurnal()
 #PressureKDE(y_data_log=True,norm_conc=True)
-#AttenuationSubslab()
+AttenuationSubslab()
 #Modeling()
 #IndianapolisTime()
-AirExchangeRateKDE()
-
-"""
-df = pd.DataFrame({'x': [0,2], 'y': [1,4]})
-df2 = pd.DataFrame({'x':[1,]})
-
-df.append(df2)
-df = df.append(df2)
-
-df.sort_values(by='x',inplace=True)
-
-df.interpolate(inplace=True)
-print(df)
-"""
+#AirExchangeRateKDE()
